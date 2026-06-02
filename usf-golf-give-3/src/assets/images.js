fdde124cdd0fe755d0ec16cae4cd4efb735b18f5
jaival-p <jaival@usf.edu>
2026-06-01 21:40:42 -0400

    Update images.js

--- usf-golf-give-3/src/assets/images.js
+++ usf-golf-give-3/src/assets/images.js
@@ -2,10 +2,18 @@
 // To use local files: add the images to this folder and switch to import statements like:
 // import usfLogo from './usf-logo.png';
 
-export const usfLogo = 'https://media.base44.com/images/public/6a0ca64f419d5b9f8c59065c/8d684b0b8_images.png';
-export const paradiseLogo = 'https://media.base44.com/images/public/6a0ca64f419d5b9f8c59065c/e1d91e5b5_paradisegolflogo.png';
-export const instagramIcon = 'https://media.base44.com/images/public/6a0ca64f419d5b9f8c59065c/6f6ee9d92_black-and-white-outline-instagram-app-logo-icon-7017516951369307x990iaok2.png';
-export const linkedinIcon = 'https://media.base44.com/images/public/6a0ca64f419d5b9f8c59065c/756e2f543_png-clipartlinkedin-logo-png-black-and-white-round-icon-circle-vector-transparent-social-media-symbol6577175-thumbnail.png';
-export const heroBackground = 'https://media.base44.com/images/public/6a0ca64f419d5b9f8c59065c/f81e1de37_golfingbackgroundimage.jpg';
-export const facebookIcon = 'https://media.base44.com/images/public/6a0ca64f419d5b9f8c59065c/ff0a5bd7e_imgbin-computer-icons-facebook-f8-lsx-world-congress-usa-2018-like-button-facebook-C5aYzyCSZ477t6yCCig0eU7t2_t.jpg';
-export const coursePhoto = 'https://media.base44.com/images/public/6a0ca64f419d5b9f8c59065c/41397d11f_photo-1587174486073-ae5e5cff23aa.jpeg';
+import usfLogoFile from './usfgolf logo.png';
+import paradiseLogoFile from './paradise golf logo.png';
+import instagramIconFile from './instagram logo.png';
+import linkedinIconFile from './linkedin logo.png';
+import facebookIconFile from './facebook logo.jpg';
+import heroBackgroundFile from './golfing background image.jpeg';
+import coursePhotoFile from './course photo.jpeg';
+
+export const usfLogo = usfLogoFile;
+export const paradiseLogo = paradiseLogoFile;
+export const instagramIcon = instagramIconFile;
+export const linkedinIcon = linkedinIconFile;
+export const facebookIcon = facebookIconFile;
+export const heroBackground = heroBackgroundFile;
+export const coursePhoto = coursePhotoFile;
